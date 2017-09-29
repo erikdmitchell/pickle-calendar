29ba7e66291c6ea9923ad808e877684747b6755b
Erik <erikdmitchell@gmail.com>
2017-09-29 11:07:50 -0400

    export json file for settings

--- admin.php
+++ admin.php
@@ -5,6 +5,7 @@ class Pickle_Calendar_Admin {
 	public function __construct() {
 		add_action('admin_enqueue_scripts', array($this, 'admin_scripts_styles'));
 		add_action('admin_menu', array($this, 'admin_menu'));
+		add_action( 'admin_init', array($this, 'process_settings_export'));
 		add_action('admin_init', array($this, 'update_settings'));
 	}
 
@@ -19,82 +20,115 @@ class Pickle_Calendar_Admin {
 	}
 	
 	public function settings_page() {
-		$html='';
+		?>
 
-		$html.='<div class="wrap">';
-			$html.='<h1>Pickle Calendar</h1>';
+		<div class="wrap">
+			<h1>Pickle Calendar</h1>
 			
-			$html.='<form class="pickle-calendar-settings-form" action="" method="post">';
-				$html.=wp_nonce_field('update_settings', 'pickle_calendar_admin', true, false);	
+			<form class="pickle-calendar-settings-form" action="" method="post">
+				<?php wp_nonce_field('update_settings', 'pickle_calendar_admin', true); ?>	
 				
-				$html.='<h2>General</h2>';
+				<h2>General</h2>
 				
-				$html.='<table class="form-table">';
-					$html.='<tbody>';
+				<table class="form-table">
+					<tbody>
 					
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="adminlabel">Admin Label</label></th>';
-							$html.='<td><input name="settings[adminlabel]" type="text" id="adminlabel" value="'.picklecalendar()->settings['adminlabel'].'" class="regular-text"></td>';
-						$html.='</tr>';
+						<tr>
+							<th scope="row"><label for="adminlabel">Admin Label</label></th>
+							<td><input name="settings[adminlabel]" type="text" id="adminlabel" value="<?php echo picklecalendar()->settings['adminlabel']; ?>" class="regular-text"></td>
+						</tr>
 					
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="cpt_single">Post Type Label (single)</label></th>';
-							$html.='<td><input name="settings[cpt_single]" type="text" id="cpt_single" value="'.picklecalendar()->settings['cpt_single'].'" class="regular-text"></td>';
-						$html.='</tr>';
+						<tr>
+							<th scope="row"><label for="cpt_single">Post Type Label (single)</label></th>
+							<td><input name="settings[cpt_single]" type="text" id="cpt_single" value="<?php echo picklecalendar()->settings['cpt_single']; ?>" class="regular-text"></td>
+						</tr>
 					
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="cpt_plural">Post Type Label (plural)</label></th>';
-							$html.='<td><input name="settings[cpt_plural]" type="text" id="cpt_plural" value="'.picklecalendar()->settings['cpt_plural'].'" class="regular-text"></td>';
-						$html.='</tr>';
+						<tr>
+							<th scope="row"><label for="cpt_plural">Post Type Label (plural)</label></th>
+							<td><input name="settings[cpt_plural]" type="text" id="cpt_plural" value="<?php echo picklecalendar()->settings['cpt_plural']; ?>" class="regular-text"></td>
+						</tr>
 
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="tax_single">Taxonomy Label (single)</label></th>';
-							$html.='<td><input name="settings[tax_single]" type="text" id="tax_single" value="'.picklecalendar()->settings['tax_single'].'" class="regular-text"></td>';
-						$html.='</tr>';	
+						<tr>
+							<th scope="row"><label for="tax_single">Taxonomy Label (single)</label></th>
+							<td><input name="settings[tax_single]" type="text" id="tax_single" value="<?php echo picklecalendar()->settings['tax_single']; ?>" class="regular-text"></td>
+						</tr>	
 
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="tax_plural">Taxonomy Label (plural)</label></th>';
-							$html.='<td><input name="settings[tax_plural]" type="text" id="tax_plural" value="'.picklecalendar()->settings['tax_plural'].'" class="regular-text"></td>';
-						$html.='</tr>';											
+						<tr>
+							<th scope="row"><label for="tax_plural">Taxonomy Label (plural)</label></th>
+							<td><input name="settings[tax_plural]" type="text" id="tax_plural" value="<?php echo picklecalendar()->settings['tax_plural']; ?>" class="regular-text"></td>
+						</tr>											
 					
-					$html.='</tbody>';				
-				$html.='</table>';	
+					</tbody>				
+				</table>	
 				
-				$html.='<h2>Post Details (metabox)</h2>';	
+				<h2>Post Details (metabox)</h2>	
 
-				$html.='<table class="form-table">';
-					$html.='<tbody>';
+				<table class="form-table">
+					<tbody>
 					
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="disable_editor">Disable Editor</label></th>';
-							$html.='<td><label for="disable_editor"><input name="settings[disable_editor]" type="checkbox" id="disable_editor" value="1" '.checked(picklecalendar()->settings['disable_editor'], 1, false).'>Disable Editor</label>';
-						$html.='</tr>';
+						<tr>
+							<th scope="row"><label for="disable_editor">Disable Editor</label></th>
+							<td><label for="disable_editor"><input name="settings[disable_editor]" type="checkbox" id="disable_editor" value="1" <?php checked(picklecalendar()->settings['disable_editor'], 1); ?>>Disable Editor</label>
+						</tr>
 					
-						$html.='<tr>';
-							$html.='<th scope="row"><label for="include_details">Details Box</label></th>';
-							$html.='<td><label for="include_details"><input name="settings[include_details]" type="checkbox" id="include_details" value="1" '.checked(picklecalendar()->settings['include_details'], 1, false).'>Show Details Box</label>';
-						$html.='</tr>';
+						<tr>
+							<th scope="row"><label for="include_details">Details Box</label></th>
+							<td><label for="include_details"><input name="settings[include_details]" type="checkbox" id="include_details" value="1" <?php checked(picklecalendar()->settings['include_details'], 1); ?>>Show Details Box</label>
+						</tr>
 					
-						$html.='<tr class="details-box">';
-							$html.='<th scope="row"><label for="start_date">Show Start Date</label></th>';
-							$html.='<td><label for="start_date"><input name="settings[detail_options][start_date]" type="checkbox" id="start_date" value="1" '.checked(picklecalendar()->settings['detail_options']['start_date'], 1, false).'>Show Start Date Box</label>';
-						$html.='</tr>';
+						<tr class="details-box">
+							<th scope="row"><label for="start_date">Show Start Date</label></th>
+							<td><label for="start_date"><input name="settings[detail_options][start_date]" type="checkbox" id="start_date" value="1" <?php checked(picklecalendar()->settings['detail_options']['start_date'], 1); ?>>Show Start Date Box</label>
+						</tr>
 					
-						$html.='<tr class="details-box">';
-							$html.='<th scope="row"><label for="end_date">Show End Date</label></th>';
-							$html.='<td><label for="end_date"><input name="settings[detail_options][end_date]" type="checkbox" id="end_date" value="1" '.checked(picklecalendar()->settings['detail_options']['end_date'], 1, false).'>Show End Date Box</label>';
-						$html.='</tr>';											
+						<tr class="details-box">
+							<th scope="row"><label for="end_date">Show End Date</label></th>
+							<td><label for="end_date"><input name="settings[detail_options][end_date]" type="checkbox" id="end_date" value="1" <?php checked(picklecalendar()->settings['detail_options']['end_date'], 1); ?>>Show End Date Box</label>
+						</tr>											
 					
-					$html.='</tbody>';				
-				$html.='</table>';	
+					</tbody>				
+				</table>	
 				
-				$html.='<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>';
+				<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
 				
-			$html.='</form>';
+			</form>
 			
-		$html.='</div>';
+			<div class="metabox-holder">
+				<div class="postbox">
+					<h3><span><?php _e( 'Export Settings' ); ?></span></h3>
+					<div class="inside">
+						<p><?php _e( 'Export the plugin settings for this site as a .json file. This allows you to easily import the configuration into another site.' ); ?></p>
+						<form method="post">
+							<p><input type="hidden" name="pc_action" value="export_settings" /></p>
+							<p>
+								<?php wp_nonce_field('pickle_calendar_export_nonce', 'pickle_calendar_export_nonce'); ?>
+								<?php submit_button( __( 'Export' ), 'secondary', 'submit', false ); ?>
+							</p>
+						</form>
+					</div><!-- .inside -->
+				</div><!-- .postbox -->
 	
-		echo $html;
+				<div class="postbox">
+					<h3><span><?php _e( 'Import Settings' ); ?></span></h3>
+					<div class="inside">
+						<p><?php _e( 'Import the plugin settings from a .json file. This file can be obtained by exporting the settings on another site using the form above.' ); ?></p>
+						<form method="post" enctype="multipart/form-data">
+							<p>
+								<input type="file" name="import_file"/>
+							</p>
+							<p>
+								<input type="hidden" name="pc_action" value="import_settings" />
+								<?php wp_nonce_field( 'pickle_calendar_import_nonce', 'pickle_calendar_import_nonce' ); ?>
+								<?php submit_button( __( 'Import' ), 'secondary', 'submit', false ); ?>
+							</p>
+						</form>
+					</div><!-- .inside -->
+				</div><!-- .postbox -->
+			</div><!-- .metabox-holder -->
+
+		</div><!-- wrap -->
+		
+		<?php
 	}
 	
 	public function update_settings() {
@@ -123,6 +157,31 @@ class Pickle_Calendar_Admin {
 		wp_redirect(site_url($_POST['_wp_http_referer']));
 		exit;
 	}
+	
+	public function process_settings_export() {
+		if (empty($_POST['pc_action']) || $_POST['pc_action']!='export_settings')
+			return;
+					
+		if (!isset($_POST['pickle_calendar_export_nonce']) || !wp_verify_nonce($_POST['pickle_calendar_export_nonce'], 'pickle_calendar_export_nonce'))
+			return;
+
+		if (!current_user_can('manage_options'))
+			return;
+			
+		$settings=get_option('pickle_calendar_settings');
+		
+		ignore_user_abort(true);
+		
+		nocache_headers();
+		
+		header('Content-Type: application/json; charset=utf-8');
+		header('Content-Disposition: attachment; filename=pickle-calendar-settings-export-'.date('m-d-Y').'.json');
+		header("Expires: 0");
+		
+		echo json_encode($settings);
+		
+		exit;
+	}	
 	
 }	
 

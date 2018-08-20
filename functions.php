d986e5edd50ff75cc4dbd9dd5754b4505d810592
Erik <erikdmitchell@gmail.com>
2018-08-20 14:05:26 -0400

    added get events (posts) function
    fixes #14

--- functions.php
+++ functions.php
@@ -43,62 +43,88 @@ function pc_get_event_details( $the_post = '' ) {
     return $dates;
 }
 
+/**
+ * PC get posts.
+ * 
+ * @access public
+ * @param mixed $args (default: null).
+ * @return array
+ */
 function pc_get_posts( $args = null ) {
     global $wpdb;
 
+    $posts = array();
+    $today = date('Y-m-d');
     $defaults = array(
-        'numberposts' => 5,
-        'category' => 0,
-        'orderby' => 'date',
-        'order' => 'DESC',
-        'include' => array(),
-        'exclude' => array(),
-        'meta_key' => '',
-        'meta_value' => '',
-        'post_type' => 'post',
-        'suppress_filters' => true,
+        'events' => 5,
+        'orderby' => 'start_date',
+        'order' => 'ASC',
+        'start_date' => '',
+        'end_date' => '',
     );
 
-    $r = wp_parse_args( $args, $defaults );
-
-    /*
-    $today = date('Y-m-d');
-    $posts = $wpdb->get_results("
-        SELECT wp_posts.ID,
-        wp_posts.post_title,
-        wp_postmeta.*
-        FROM $wpdb->posts
-        INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id )
-        WHERE $wpdb->posts.post_type = 'pcevent'
-        AND $wpdb->posts.post_status = 'publish'
-        AND ( $wpdb->postmeta.meta_key LIKE '_start_date_%' AND CAST($wpdb->postmeta.meta_value AS DATE) >= '$today')
-        ORDER BY $wpdb->postmeta.meta_value ASC
-    ");
-    */
-
-    /*
-        $post_ids = $wpdb->get_col(
-            $wpdb->prepare(
-                "
-            SELECT wp_posts.ID
-            FROM $wpdb->posts
-            INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id )
-            INNER JOIN $wpdb->postmeta AS mt1 ON ( $wpdb->posts.ID = mt1.post_id )
-            WHERE 1=1
-                AND ( ( $wpdb->postmeta.meta_key LIKE %s AND CAST($wpdb->postmeta.meta_value AS DATE) <= %s ) AND ( mt1.meta_key LIKE %s AND CAST(mt1.meta_value AS DATE) >= %s ) )
-                AND ( REPLACE($wpdb->postmeta.meta_key, '_start_date_', '') = REPLACE(mt1.meta_key, '_end_date_', '') )
-                AND $wpdb->posts.post_type = 'pcevent'
-                AND (($wpdb->posts.post_status = 'publish'))
-            GROUP BY $wpdb->posts.ID
-            ORDER BY $wpdb->postmeta.meta_value ASC
-            ",
-                '_start_date_%',
-                $date,
-                '_end_date_%',
-                $date
-            )
-        );
-    */
+    $args = wp_parse_args( $args, $defaults );
+    $select = "SELECT wp_posts.ID";
+    $db_from = "FROM $wpdb->posts INNER JOIN $wpdb->postmeta ON ( $wpdb->posts.ID = $wpdb->postmeta.post_id ) INNER JOIN $wpdb->postmeta AS mt1 ON ( $wpdb->posts.ID = mt1.post_id )";
+    $where_data = array("1=1", "AND $wpdb->posts.post_type = 'pcevent'", "AND (($wpdb->posts.post_status = 'publish'))", "AND ( REPLACE($wpdb->postmeta.meta_key, '_start_date_', '') = REPLACE(mt1.meta_key, '_end_date_', '') )");
+    
+    // this is where our "options are run".
+    // anything from today onwards (default) -- CAN SET DATE FOR START DATE
+    if (!empty($args['start_date']) && empty($args['end_date'])) :
+        $where_data[] = "AND ( ( mt1.meta_key LIKE '_end_date_%' AND CAST(mt1.meta_value AS DATE) >= {$args['start_date']} ) )"; // anything from start date onwards.
+    elseif (!empty($args['end_date']) && empty($args['start_date'])) :
+        $where_data[] = "AND ( ( mt1.meta_key LIKE '_end_date_%' AND CAST(mt1.meta_value AS DATE) <= $today ) )"; // anything before today.
+    elseif (!empty($args['start_date']) && !empty($args['end_date'])) :
+        $where_data[] = "AND ( ( $wpdb->postmeta.meta_key LIKE '_start_date_%' AND CAST($wpdb->postmeta.meta_value AS DATE) >= {$args['start_date']} ) AND ( mt1.meta_key LIKE '_end_date_%' AND CAST(mt1.meta_value AS DATE) <= {$args['end_date']} ) )"; // between two dates.
+    else :
+        $where_data[] = "AND ( ( mt1.meta_key LIKE '_end_date_%' AND CAST(mt1.meta_value AS DATE) >= $today ) )"; // anything from today onwards (default).
+    endif;
+    
+    $where = implode(' ', $where_data);
+    $post_ids = $wpdb->get_col("
+		$select
+		$db_from
+		WHERE $where
+		GROUP BY $wpdb->posts.ID
+		ORDER BY $wpdb->postmeta.meta_value {$args['order']}
+		LIMIT {$args['events']}        
+    ");  
+    
+    foreach ($post_ids as $post_id) :
+        $post = get_post($post_id);
+        $post->event_dates = pc_get_event_dates( $post_id );
+        
+        $posts[] = $post;
+    endforeach;
 
     return $posts;
 }
+
+/**
+ * Get event dates.
+ * 
+ * @access public
+ * @param int $post_id (default: 0).
+ * @return array
+ */
+function pc_get_event_dates( $post_id = 0 ) {
+    $dates = array();
+    $meta = get_post_meta( $post_id );
+
+    foreach ( $meta as $key => $value ) :
+        if ( strpos( $key, '_start_date_' ) !== false ) :
+            if ( isset( $value[0] ) ) :
+                preg_match( '/([0-9]+)/', $key, $matches );
+                $dates[ $matches[1] ]['start_date'] = $value[0];
+            endif;
+        elseif ( strpos( $key, '_end_date_' ) !== false ) :
+            if ( isset( $value[0] ) ) :
+                preg_match( '/([0-9]+)/', $key, $matches );
+                $dates[ $matches[1] ]['end_date'] = $value[0];
+            endif;
+        endif;
+    endforeach;
+
+    return $dates;
+}
+
--- templates/single-pcevent.php
+++ templates/single-pcevent.php
@@ -51,5 +51,7 @@ get_header(); ?>
     <?php get_sidebar(); ?>
 </div><!-- .wrap -->
 
+<?php pc_get_posts(); ?>
+
 <?php
 get_footer();
